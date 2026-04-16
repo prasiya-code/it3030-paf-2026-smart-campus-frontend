c6eb9db50c57da79811b933bcf82ff19195a2cd2
Inupama <inupamacaldera@gmail.com>
2026-04-16 23:54:16 +0530

    Resolved App.jsx merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { AuthProvider } from './context/AuthContext';
+import AppRoutes from './routes/AppRoutes';
+
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className="bg-black text-white min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold text-green-400">
         Smart Campus Frontend 🚀
       </h1>
     </div>
+=======
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
+>>>>>>> Stashed changes
   );
 }
 
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "../components/auth/ProtectedRoute";
+import LoginPage from "../pages/auth/LoginPage";
+import NotificationsPage from "../pages/notifications/NotificationsPage";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route 
+          path="/" 
+          element={
+            <ProtectedRoute>
+              <h1>Home</h1>
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/notifications" 
+          element={
+            <ProtectedRoute>
+              <NotificationsPage />
+            </ProtectedRoute>
+          } 
+        />
       </Routes>
     </BrowserRouter>
   );
